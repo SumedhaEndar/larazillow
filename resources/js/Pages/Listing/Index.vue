607ecb75bad52a4731199f5b451fce32a140f585
SumedhaEndar <1211200546@student.mmu.edu.my>
2023-08-10 05:01:21 +0800

    Listing Details (Non-Prop Attributes)

--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -4,7 +4,8 @@
             <div>
                 <!-- <Link :href="`/listing/${listing.id}`"> -->
                 <Link :href="route('listing.show', {listing: listing.id})">
-                    <ListingAddress :listing="listing"/>
+                    <ListingSpace :listing="listing" class="text-lg"/>
+                    <ListingAddress :listing="listing" class="text-gray-500"/>
                 </Link>
             </div>
             <div>
@@ -22,6 +23,7 @@
 <script setup>
     import {Link} from '@inertiajs/vue3'
     import ListingAddress from '../../Components/ListingAddress.vue';
+    import ListingSpace from '../../Components/ListingSpace.vue';
     import Box from '../../Components/UI/Box.vue';
     defineProps(
         {
