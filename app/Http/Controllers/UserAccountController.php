f01145cfbbb942f3ce461846e1be2bf640b601c0
SumedhaEndar <1211200546@student.mmu.edu.my>
2023-08-13 17:04:59 +0800

    User Registration Logic

--- app/Http/Controllers/UserAccountController.php
+++ app/Http/Controllers/UserAccountController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserAccountController extends Controller
 {
@@ -10,4 +13,16 @@ class UserAccountController extends Controller
     {
         return inertia('UserAccount/Create');
     }   
+
+    public function store(Request $request){
+        // New laravel already hashed the password by default
+        $user = User::create($request->validate([
+            'name'=> 'required',
+            'email'=> 'required|email|unique:users',
+            'password'=> 'required|min:8|confirmed'
+        ]));
+        Auth::login($user);
+
+        return redirect()->route('listing.index')->with('success','Account created!');
+    }
 }
